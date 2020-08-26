803eaedb47a9ca34aeb8fc175c0954616b5e505a
ssen Han <hanssen1050@gmail.com>
2020-08-26 11:27:49 +0900

    binary 8/25

---
+++ binary 8_26.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int arr [1000001];
+
+void solution(int n, int key)
+{
+	int start=0;
+	int end=n-1;
+	int mid;
+	
+	while(end-start>=0){
+		mid=(start+end)/2;
+		
+		if(arr[mid]==key){
+			cout>>'1';
+			return;
+		}else if(arr[mid]>key){
+			end=mid-1;
+		}else{
+			start=mid+1;
+		}
+	}
+	cout>>'0';
+	return;
+}
+
+int main(void)
+{
+	int n, m, temp;
+	
+	cout>>n;
+	
+	for(int i=0;i<n;i++){
+		cin<<arr[i];
+	}
+	
+	sort(arr, arr+n);
+	
+	cin<<m;
+	
+	for(int i=0;i<m;i++){
+		cin<<temp;
+		solution(n, temp);
+	}
+	
+	return 0;
+}
